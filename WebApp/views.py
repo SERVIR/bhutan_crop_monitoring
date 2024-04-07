cdc1aafe7a5c7d2ec5be1a06f93bddff80a75150
Billy Ashmall <billy.ashmall@nasa.gov>
2024-04-07 10:36:44 -0500

    Average rainfall

--- WebApp/views.py
+++ WebApp/views.py
@@ -53,9 +53,11 @@ def dashboard(request):
         {"hasVisualization": True, "ui_id": 'maize_', "title": "Maize Map", "url": "https://thredds.servirglobal.net/thredds/wms/agg/crop/bhutan/maize.nc4?service=WMS&version=1.3.0&", "attribution": "BillyZ 2",
          "layers": "maize", "default_style": "boxfill/maize", "default_color_range": ".5,1", "overrange": "transparent",
          "belowrange": "transparent", "default_year": "2016", "default_on": True},
-        {"hasVisualization": True, "ui_id": 'crop_land_', "title": "Crop land extent ", "url": "https://csthredds.servirglobal.net/thredds/wms/Agg/ucsb-chirps_global_0.05deg_daily.nc4?service=WMS&version=1.3.0", "attribution": "BillyZ 3",
+        {"hasVisualization": True, "ui_id": 'crop_land_', "title": "Crop land extent ", "url": "https://csthredds.servirglobal.net/thredds/wms/Agg/ucsb-chirps_global_0.05deg_daily.nc4?service=WMS&version=1.3.0&", "attribution": "BillyZ 3",
          "layers": "precipitation_amount", "default_style": "boxfill/cape_surface", "default_color_range": "1,50", "overrange": "extend",
-         "belowrange": "extend", "default_year": "2016", "default_on": False}]}
+         "belowrange": "extend", "default_year": "2016", "default_on": False},{"hasVisualization": True, "ui_id": 'average_precipitation_', "title": "Average Precipitation", "url": "https://thredds.servirglobal.net/thredds/wms/agg/crop/global/ucsb-chirps_global_0.05deg_daily_accumulation.nc4?service=WMS&version=1.3.0&", "attribution": "BillyZ 3",
+         "layers": "precipitation_monthly_avg", "default_style": "boxfill/cape_surface", "default_color_range": "1,40", "overrange": "transparent",
+         "belowrange": "transparent", "default_year": "2016", "default_month": "01", "default_on": False}]}
 
     return render(request, 'WebApp/dashboard.html', context)
 
